1cc7b605233fe19c49e0d48fdaf3c197e8436031
sofecia <lourenso.sofecia@gmail.com>
2022-11-16 23:25:20 +0100

    component loader feito

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import  ReactDOM  from 'react-dom';
 import styles from "./Loader.module.scss";
 import loaderImg from "../../assets/loader.git";
 
 const Loader = () => {
-  return (
+  return ReactDOM.createPortal (
     <div className = {styles.wrapper}>
       <div className = {styles.loader}>
-        <img src = "" alt = "Loading" />
+        <img src = {loaderImg} alt = "Loading" />
       </div>
-    </div>
+    </div>,
+    document.getElementById("loader")
   );
 };
 
