047483d586c51d054f0b4cb55bce0958e370b45c
sofecia <lourenso.sofecia@gmail.com>
2023-01-05 13:17:00 +0100

    Apresentar pra vaga de emprego!

--- src/components/Slider/Slider-data.js
+++ src/components/Slider/Slider-data.js
@@ -1,17 +1,8 @@
 export const sliderData = [
-    {
-      image: "https://i.ibb.co/TYJ2QMd/langerie-black-back.jpg",
-      heading: "Shoes Villa",
-      desc: "Up to 30% off on all onsale proucts.",
-    },
-    {
-      image: "https://i.ibb.co/MSpQ506/langerie-black-front.jpg",
-      heading: "Women Fashion",
-      desc: "Up to 30% off on all onsale proucts.",
-    },
+    
     {
       image: "https://i.ibb.co/HXjD3V0/bg-2.jpg",
-      heading: "Men Fashion",
+      heading: "Shoes Villa",
       desc: "Up to 30% off on all onsale proucts.",
     },
     {
@@ -24,3 +15,13 @@ export const sliderData = [
 /** Imagens vindo da Net */
 //<a href="https://ibb.co/n64mXMt"><img src="https://i.ibb.co/TYJ2QMd/langerie-black-back.jpg" alt="langerie-black-back" border="0"></a>
 //<a href="https://ibb.co/yP57BjS"><img src="https://i.ibb.co/MSpQ506/langerie-black-front.jpg" alt="langerie-black-front" border="0"></a><br /><a target='_blank' href='https://imgbb.com/'>share picture</a><br />
+/*{
+      image: "https://i.ibb.co/TYJ2QMd/langerie-black-back.jpg",
+      heading: "Women Fashion Langerie back",
+      desc: "Up to 30% off on all onsale proucts.",
+    },
+    {
+      image: "https://i.ibb.co/MSpQ506/langerie-black-front.jpg",
+      heading: "Women Fashion Langerie front",
+      desc: "Up to 30% off on all onsale proucts.",
+    },*/
